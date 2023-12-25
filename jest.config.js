08a68991796ec3443a700c305d19ff26bb363a89
ashwin-santhanam <ashwinsanthanam098@gmail.com>
2023-12-25 11:37:01 +0530

    Use json file instead of js file for defining jest configuration

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    transform: {'^.+\\.ts?$': 'ts-jest'},
-    testEnvironment: 'node',
-    testRegex: '/tests/.*(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-  };
